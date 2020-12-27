256a3f2f2c60ca81cca73847dfa6b8aec8d12e76
Daniel Ribeiro <drsantosribeiro@gmail.com>
2020-12-26 23:51:28 -0300

    Finalização Capítulo 1

---
+++ SolucaoCapitulo01-RegistrandoGastoRestaurante/RegistrandoGastoRestaurante/frmGastoEmRestaurante.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace RegistrandoGastoRestaurante
+{
+    partial class frmGastoEmRestaurante
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDespesa = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.txtDespesa = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtTotalDaConta = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblDespesa
+            // 
+            this.lblDespesa.AutoSize = true;
+            this.lblDespesa.Location = new System.Drawing.Point(12, 9);
+            this.lblDespesa.Name = "lblDespesa";
+            this.lblDespesa.Size = new System.Drawing.Size(52, 13);
+            this.lblDespesa.TabIndex = 0;
+            this.lblDespesa.Text = "Despesa:";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.ForeColor = System.Drawing.Color.Blue;
+            this.lblTotal.Location = new System.Drawing.Point(12, 37);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(94, 13);
+            this.lblTotal.TabIndex = 1;
+            this.lblTotal.Text = "Total com 10%:";
+            // 
+            // txtDespesa
+            // 
+            this.txtDespesa.Location = new System.Drawing.Point(70, 5);
+            this.txtDespesa.Name = "txtDespesa";
+            this.txtDespesa.Size = new System.Drawing.Size(100, 20);
+            this.txtDespesa.TabIndex = 2;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(176, 4);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtTotalDaConta
+            // 
+            this.txtTotalDaConta.BackColor = System.Drawing.Color.Yellow;
+            this.txtTotalDaConta.Enabled = false;
+            this.txtTotalDaConta.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotalDaConta.Location = new System.Drawing.Point(112, 33);
+            this.txtTotalDaConta.Name = "txtTotalDaConta";
+            this.txtTotalDaConta.Size = new System.Drawing.Size(139, 20);
+            this.txtTotalDaConta.TabIndex = 4;
+            // 
+            // frmGastoEmRestaurante
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 62);
+            this.Controls.Add(this.txtTotalDaConta);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtDespesa);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblDespesa);
+            this.Name = "frmGastoEmRestaurante";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gasto em restaurante";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDespesa;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.TextBox txtDespesa;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtTotalDaConta;
+    }
+}
---
+++ SolucaoCapitulo01-RestoDaDivisão/RestoDaDivisão/FormRestoDeDivisao.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RestoDaDivisão
+{
+    public partial class frmRestoDeDivisao : Form
+    {
+        public frmRestoDeDivisao()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRealizarDivisao_Click(object sender, EventArgs e)
+        {
+            int dividendo = Convert.ToInt32(txtDividendo.Text);
+            int divisor = Convert.ToInt32(txtDivisor.Text);
+            int resto = dividendo % divisor;
+            txtResto.Text = resto.ToString();
+        }
+    }
+}
